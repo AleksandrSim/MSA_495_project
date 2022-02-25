7335887ad8b489daeee6b6f7a8248aeda381a7a8
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-02-24 18:23:41 -0600

    Add files via upload

--- binary_classification/model.py
+++ binary_classification/model.py
@@ -1,5 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
+
+
+
 class Simple(nn.Module):
     def __init__(self):
         super(Simple, self).__init__()
@@ -9,7 +12,7 @@ class Simple(nn.Module):
 
         self.maxPool = nn.MaxPool2d(2,2)
 
-        self.Linear1 = nn.Linear(41772, 1000)
+        self.Linear1 = nn.Linear(112908, 1000)
         self.activation = nn.Tanh()
         self.Linear = nn.Linear(1000, 500)
         self.sigmoid = nn.Sigmoid()
@@ -21,7 +24,6 @@ class Simple(nn.Module):
     def forward(self, x):
         x = self.maxPool(self.conv1(x))
         x = self.maxPool(self.conv2(x))
-
         x = x.view(x.shape[0],-1)
         x = self.activation(self.Linear1(x))
         x = self.activation(self.Linear2(x))
@@ -34,14 +36,14 @@ class Medium(nn.Module):
     def __init__(self):
         super(Medium, self).__init__()
         # convolutional layer
-        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.conv1 = nn.Conv2d(3, 7, 5)
         # max pooling layer
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.conv2 = nn.Conv2d(7, 10 , 5)
         self.dropout = nn.Dropout(0.2)
-        self.fc1 = nn.Linear(111392, 256)
+        self.fc1 = nn.Linear(10*97*97, 256)
         self.fc2 = nn.Linear(256, 84)
-        self.fc3 = nn.Linear(84, 2)
+        self.fc3 = nn.Linear(84, 5)
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
@@ -49,10 +51,70 @@ class Medium(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.dropout(x)
-        x = x.view(-1, 111392)
+        x = x.view(x.shape[0],-1)
         x = F.relu(self.fc1(x))
         x = self.dropout(F.relu(self.fc2(x)))
-        x = self.softmax(self.fc3(x))
+        x = self.fc3(x)
         return x
 
 
+class AgeAlexNet(nn.Module):
+    def __init__(self, pretrained=False, modelpath=None):
+        super(AgeAlexNet, self).__init__()
+        assert pretrained is False or modelpath is not None, "pretrain model need to be specified"
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.LocalResponseNorm(2,2e-5,0.75),
+
+            nn.Conv2d(96, 256, kernel_size=5, stride=1,groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.LocalResponseNorm(2, 2e-5, 0.75),
+
+            nn.Conv2d(256, 384, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(384, 384, kernel_size=3,stride=1,groups=2),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(384, 256, kernel_size=3,stride=1,groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.age_classifier=nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(12544, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 5),
+        )
+        if pretrained is True:
+            self.load_pretrained_params(modelpath)
+
+        self.Conv3_feature_module=nn.Sequential()
+        self.Conv4_feature_module=nn.Sequential()
+        self.Conv5_feature_module=nn.Sequential()
+        self.Pool5_feature_module=nn.Sequential()
+        for x in range(10):
+            self.Conv3_feature_module.add_module(str(x), self.features[x])
+        for x in range(10,12):
+            self.Conv4_feature_module.add_module(str(x),self.features[x])
+        for x in range(12,14):
+            self.Conv5_feature_module.add_module(str(x),self.features[x])
+        for x in range(14,15):
+            self.Pool5_feature_module.add_module(str(x),self.features[x])
+
+
+    def forward(self, x):
+        self.conv3_feature=self.Conv3_feature_module(x)
+        self.conv4_feature=self.Conv4_feature_module(self.conv3_feature)
+        self.conv5_feature=self.Conv5_feature_module(self.conv4_feature)
+        pool5_feature=self.Pool5_feature_module(self.conv5_feature)
+        self.pool5_feature=pool5_feature
+        flattened = pool5_feature.view(pool5_feature.size(0), -1)
+        age_logit = self.age_classifier(flattened)
+        return age_logit
--- binary_classification/train_pipeline.py
+++ binary_classification/train_pipeline.py
@@ -3,25 +3,30 @@ import torch
 from torch import nn
 import os
 from dataloader import BinaryClass
-from model import Simple, Medium
+from model import Simple, Medium, AgeAlexNet
 from torch.utils.data import DataLoader
-
+import numpy as np
 
 if __name__ =='__main__':
     path_main =os.path.split(os.getcwd())[0]
-    df = pd.read_csv(path_main + '/files/cleaned_images.csv')
+    df = pd.read_csv(path_main+'/files/train.txt', sep=' ', header = None)
+    df.columns =['name','class']
  #   df = df.sample(frac=1).reset_index(drop=True)
     #df_filtered = df[['image_name','old_young']]
     #print(df_filtered)
-    path_to_images = '/Users/aleksandrsimonyan/Desktop/CACD2000/' #make this dinamic and change the path obviously
+    path_to_images = '/Users/aleksandrsimonyan/Desktop/cross_age_dataset_cleaned_and_resized/' #make this dinamic and change the path
     #net = Simple()
+    #net = Simple()
+  #  net =  AgeAlexNet(pretrainded=False)
     net = Simple()
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
     dataset = BinaryClass(df, path_to_images)
-    dataset = torch.utils.data.DataLoader(dataset, batch_size= 1000)
-    for epoch in range(4):
+    dataset = torch.utils.data.DataLoader(dataset, batch_size= 100)
+    overall_loss = []
+    for epoch in range(100):
         running_loss = 0.0
+
         for i,k in enumerate(dataset):
             X,y = k
             optimizer.zero_grad()
@@ -33,10 +38,15 @@ if __name__ =='__main__':
             running_loss += loss.item()
             if i %100 == 99:
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
+                overall_loss.append(loss.item())
+                torch.save(net.state_dict(), path_main + '/binary_classification/models/medium' + str(i) +'.pt')
+                print('fin')
         print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
 
         running_loss =0.0
     print('finished_training' + str(loss.item()))
+    print('mean_loss' + str(np.mean(overall_loss)))
+
 
 
 
