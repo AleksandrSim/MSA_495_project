1029f5c5dd8316d76cde4c631e039abd8ae7e17c
baljash-cheema <baljash-cheema@northwestern.edu>
2022-02-28 11:21:51 -0600

    fixed bug in prediction.py

--- binary_classification/predictions.py
+++ binary_classification/predictions.py
@@ -39,16 +39,22 @@ for i in classes_to_covert:
 df['3_class'] = np.array(new)
 df = df.drop(['class'], axis=1)
 df.columns = ['name', 'class']
+
+# train, valid = train_test_split(df, test_size=0.2, random_state=42)
+# train = train.reset_index(drop=True)
+# valid = train.reset_index(drop=True)
+
 train, valid = train_test_split(df, test_size=0.2, random_state=42)
-train = train.reset_index(drop=True)
-valid = train.reset_index(drop=True)
+train = df.sample(frac=0.8, random_state=200)
+valid = df.drop(train.index)
+
 classes = [0, 1, 2]
 correct_pred = {classname: 0 for classname in classes}
 total_pred = {classname: 0 for classname in classes}
 path_to_images = '/Users/aleksandrsimonyan/Desktop/cross_age_dataset_cleaned_and_resized/'  # make this dinamic and change the path
 
 val_dataset = BinaryClass(valid, path_to_images)
-val_dataset = torch.utils.data.DataLoader(val_dataset, batch_size=100)
+val_dataset = torch.utils.data.DataLoader(val_dataset, batch_size=100,train=False)
 # again no gradients needed
 with torch.no_grad():
     for i, data in enumerate(val_dataset):
--- binary_classification/train_pipeline.py
+++ binary_classification/train_pipeline.py
@@ -35,67 +35,67 @@ if __name__ == '__main__':
 
     # # train = train.reset_index(drop=True)
     # # valid = train.reset_index(drop=True)
-    #
-    # #   df = df.sample(frac=1).reset_index(drop=True)
-    # # df_filtered = df[['image_name','old_young']]
-    # # print(df_filtered)  # make this dinamic and change the path
-    # # net = Simple()
-    # # net = Simple()
-    # model_conv = torchvision.models.vgg16_bn(pretrained=True)
-    # #num_ftrs = model_conv.classifier.
-    # for param in model_conv.parameters():
-    #     param.requires_grad = False
-    # model_conv.classifier = nn.Sequential(
-    #         nn.Linear(25088 , 512),
-    #         nn.BatchNorm1d(512),
-    #         nn.Dropout(0.2),
-    #         nn.Linear(512 , 256),
-    #         nn.Linear(256 , 3)
-    #     )
-    # net = model_conv
-    #
-    #
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-    # dataset = BinaryClass(train, original_images_path)
-    # dataset = torch.utils.data.DataLoader(dataset, batch_size=100)
-    # val_dataset = BinaryClass(valid, original_images_path)
-    # val_dataset = torch.utils.data.DataLoader(val_dataset, batch_size=100)
-    # overall_loss = []
-    #
-    # for epoch in range(10):
-    #     running_loss = 0.0
-    #
-    #     for i, k in enumerate(dataset):
-    #         X, y = k
-    #         optimizer.zero_grad()
-    #         predictions = net(X)
-    #         loss = criterion(predictions, y)
-    #         loss.backward()
-    #         optimizer.step()
-    #
-    #         running_loss += loss.item()
-    #         if i % 10 == 9:
-    #             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
-    #             overall_loss.append(loss.item())
-    #             torch.save(net.state_dict(), class_model_path_Alex + str(i) + '.pt')
-    #             print('fin')
-    #
-    #     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
-    #     correct = 0
-    #     total = 0
-    #     # since we're not training, we don't need to calculate the gradients for our outputs
-    #     with torch.no_grad():
-    #         for data in val_dataset:
-    #             images, labels = data
-    #             # calculate outputs by running images through the network
-    #             outputs = net(images)
-    #             # the class with the highest energy is what we choose as prediction
-    #             _, predicted = torch.max(outputs.data, 1)
-    #             total += labels.size(0)
-    #             correct += (predicted == labels).sum().item()
-    #     print('validation_accuracy------------->' + str(correct / total))
-    #
-    #     running_loss = 0.0
-    # print('finished_training' + str(loss.item()))
-    # print('mean_loss' + str(np.mean(overall_loss)))
+    #   df = df.sample(frac=1).reset_index(drop=True)
+    # df_filtered = df[['image_name','old_young']]
+    # print(df_filtered)  # make this dinamic and change the path
+    # net = Simple()
+    # net = Simple()
+
+    model_conv = torchvision.models.vgg16_bn(pretrained=True)
+    #num_ftrs = model_conv.classifier.
+    for param in model_conv.parameters():
+        param.requires_grad = False
+    model_conv.classifier = nn.Sequential(
+            nn.Linear(25088 , 512),
+            nn.BatchNorm1d(512),
+            nn.Dropout(0.2),
+            nn.Linear(512 , 256),
+            nn.Linear(256 , 3)
+        )
+    net = model_conv
+
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+    dataset = BinaryClass(train, original_images_path)
+    dataset = torch.utils.data.DataLoader(dataset, batch_size=100)
+    val_dataset = BinaryClass(valid, original_images_path, train=False)
+    val_dataset = torch.utils.data.DataLoader(val_dataset, batch_size=100)
+    overall_loss = []
+
+    for epoch in range(10):
+        running_loss = 0.0
+
+        for i, k in enumerate(dataset):
+            X, y = k
+            optimizer.zero_grad()
+            predictions = net(X)
+            loss = criterion(predictions, y)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 10 == 9:
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
+                overall_loss.append(loss.item())
+                # torch.save(net.state_dict(), class_model_path_Alex + str(i) + '.pt')
+                print('fin')
+
+        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
+        correct = 0
+        total = 0
+        # since we're not training, we don't need to calculate the gradients for our outputs
+        with torch.no_grad():
+            for data in val_dataset:
+                images, labels = data
+                # calculate outputs by running images through the network
+                outputs = net(images)
+                # the class with the highest energy is what we choose as prediction
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print('validation_accuracy------------->' + str(correct / total))
+
+        running_loss = 0.0
+    print('finished_training' + str(loss.item()))
+    print('mean_loss' + str(np.mean(overall_loss)))
