b96224878e427dd468ca6fb12de33a5f89338dee
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-03-01 19:37:28 -0600

    Updated every file to work with parameters, only supply the required parameters now ( your file path directory, and any other arguments required). Finally, I cleaned up the code a liitle bit, next steps -> perform further testing and improve accuracy on classificatrion model and build GANs.

--- binary_classification/predictions.py
+++ binary_classification/predictions.py
@@ -13,59 +13,62 @@ from helper_functions import *
 from sklearn.model_selection import train_test_split
 import prepare_data_training
 
-net = Simple()
-model_conv = torchvision.models.resnet18(pretrained=False)
-num_ftrs = model_conv.fc.in_features
 
-for param in model_conv.parameters():
-    param.requires_grad = False
+if __name__ == '__main__':
 
-model_conv.fc = nn.Sequential(nn.Linear(num_ftrs, 512),
-                              nn.ReLU(),
-                              nn.Dropout(0.2),
-                              nn.Linear(512, 2))
+    net = Simple()
+    model_conv = torchvision.models.resnet18(pretrained=False)
+    num_ftrs = model_conv.fc.in_features
 
-# To read the data directory from the argument given
-user_path = sys.argv[1]
-user_provided_model = sys.argv[2]
+    for param in model_conv.parameters():
+        param.requires_grad = False
 
-pretrained_model_path = user_path + class_model_path + user_provided_model
+    model_conv.fc = nn.Sequential(nn.Linear(num_ftrs, 512),
+                                  nn.ReLU(),
+                                  nn.Dropout(0.2),
+                                  nn.Linear(512, 2))
 
-if file_exists(pretrained_model_path):
-    states = torch.load(pretrained_model_path)
-else:
-    print("Please check the file directory or the parameter name you provided for the pretrained-model and try again..")
-    exit(0)
+    # To read the data directory from the argument given
+    user_path = sys.argv[1]
+    user_provided_model = sys.argv[2]
 
-model_conv.load_state_dict(states)
+    pretrained_model_path = user_path + class_model_path + user_provided_model
 
-net = model_conv
+    if file_exists(pretrained_model_path):
+        states = torch.load(pretrained_model_path)
+    else:
+        print("Please check the file directory or the parameter name you provided for the pretrained-model and try again..")
+        exit(0)
 
-train, valid, weights = prepare_data_training.get_the_df(os.path.split(os.getcwd())[0] + '/files/train.txt', class_2=True)
-print(net)
+    model_conv.load_state_dict(states)
 
-classes = [0, 1]
-correct_pred = {classname: 0 for classname in classes}
-total_pred = {classname: 0 for classname in classes}
+    net = model_conv
 
-val_dataset = BinaryClass(valid, user_path + clean_images_output_path, train=False)
-val_dataset = torch.utils.data.DataLoader(val_dataset, batch_size=100)
-# again no gradients needed
-with torch.no_grad():
-    for i, data in enumerate(val_dataset):
-        images, labels = data
-        outputs = net(images)
-        _, predictions = torch.max(outputs, 1)
-        # collect the correct predictions for each class
-        for label, prediction in zip(labels, predictions):
-            if label == prediction:
-                correct_pred[classes[label]] += 1
-            total_pred[classes[label]] += 1
-            if i % 10 == 9:
-                print('correct------->' + str(correct_pred))
-                print('overall------->' + str(total_pred))
+    train, valid, weights = prepare_data_training.get_the_df(os.path.split(os.getcwd())[0] + '/files/train.txt', class_2=True)
+    print(net)
 
-# print accuracy for each class
-for classname, correct_count in correct_pred.items():
-    accuracy = 100 * float(correct_count) / total_pred[classname]
-    print(accuracy)
+    classes = [0, 1]
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    val_dataset = BinaryClass(valid, user_path + clean_images_output_path, train=False)
+    val_dataset = torch.utils.data.DataLoader(val_dataset, batch_size=100)
+    # again no gradients needed
+    with torch.no_grad():
+        for i, data in enumerate(val_dataset):
+            images, labels = data
+            outputs = net(images)
+            _, predictions = torch.max(outputs, 1)
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+                if i % 10 == 9:
+                    print('correct------->' + str(correct_pred))
+                    print('overall------->' + str(total_pred))
+
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(accuracy)
