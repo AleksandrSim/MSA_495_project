bddee144f6d4d4e9d0e40b728d530a243c213ea8
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-03-01 20:21:41 -0600

    Implementing Gans

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     author="Dimitrios Mavrofridis, Aleksandr Simonyan, Donald Baracskay, Josh Cheema",
     author_email='dimitriosmavrofridis@gmail.com',
     packages=['MSAI_495_Project'],  # same as name
-    install_requires=["gdown", "pandas", "mat73", "matplotlib", "seaborn", "torch", "opencv-python",
-                      "imutils", "dlib"]
+    install_requires=["pandas", "mat73", "matplotlib", "seaborn", "torch", "opencv-python", "imutils"]
 )
