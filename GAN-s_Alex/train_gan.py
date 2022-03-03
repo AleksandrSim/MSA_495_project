586d64fc85ab787af2c84afb880ca6454a5dcd7e
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-03-03 00:06:03 -0600

    dataloader makes, architecture also, the training  pipeline should be modified and tested. So far has some progress.

--- GAN-s_Alex/train_gan.py
+++ GAN-s_Alex/train_gan.py
@@ -1,4 +1,4 @@
 from pytorch_lightning import Trainer
 from gan_module import AgingGAN
 model = AgingGAN()
-trainer = Trainer(max_epochs=10, auto_scale_batch_size='binsearch')
+trainer = Trainer(max_epochs=10)
