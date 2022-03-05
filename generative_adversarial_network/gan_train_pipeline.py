f45e50288fb0473c5e676fc378a02743e80b3414
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-03-04 21:24:42 -0600

    Progress & Testing on Cycle GAN

--- generative_adversarial_network/gan_train_pipeline.py
+++ generative_adversarial_network/gan_train_pipeline.py
@@ -6,12 +6,13 @@ from gan_model import *
 from gan_module import *
 from global_variables import *
 from tensorboard import program
+import torch
+
 
 parser = ArgumentParser()
 parser.add_argument('--config', default='../config_files/config.yaml', help='Config .yaml file to use for training')
 
 if __name__ == '__main__':
-
     # To read the data directory from the argument given
     args = parser.parse_args()
     with open(args.config) as file:
@@ -26,6 +27,9 @@ if __name__ == '__main__':
     url = tb.launch()
     print(f"Tensorflow listening on {url}")
 
+    config['gpus'] = torch.cuda.device_count()
+    print("Now using " + str(config['gpus']) + "gpus")
     model = AgingGAN(config)
     trainer = Trainer(max_epochs=config['epochs'], gpus=config['gpus'], auto_scale_batch_size='binsearch')
     trainer.fit(model)
+
